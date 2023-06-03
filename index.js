353d3a608602f6efa3b6660b3829eef8e03a7a71
Amanda Mondini <amandamondini4@gmail.com>
2023-06-03 14:15:50 -0400

    Cheat Detector

--- index.js
+++ index.js
@@ -27,16 +27,29 @@ async function start() {
   // Now try and complete the program.
     
   let startGame = await ask("Are you ready to play? Y or N \n");
-  if(startGame == "N" || startGame == "n"){
+  if(startGame === "N" || startGame === "n"){
     console.log("Game Ended.");
     process.exit()
-  } else {
+
+     // ?! idk why this isn't working 
+  // } else if(startGame != "Y" || startGame != "y"){
+  //   console.log("Invalid Answer. Game ended.")
+  //   process.exit()
+
+  } else{
+    
     let rangeHigh = 100;
     let rangeLow = 1;
     let guess = Math.round((rangeLow + rangeHigh) / 2);
     let userResponse = await ask(`Is your number ${guess}? Y or N \n`)
-       while(userResponse == "N" || userResponse == "n" ){
+
+    while(userResponse == "N" || userResponse == "n" ){
         let rangeModifer = await ask("Is your number lower or higher? L or H \n")
+          if(rangeHigh - rangeLow == 1){
+            console.log(`Wait a second! How can your number be higher than ${rangeLow} and lower than ${rangeHigh}?`);
+            console.log("No cheating allowed. The game has ended.");
+            process.exit();
+          }
           if(rangeModifer == "L" || rangeModifer == "l"){
             rangeHigh = guess;
             rangeLow = rangeLow;
@@ -48,11 +61,11 @@ async function start() {
             guess = Math.round((rangeLow + rangeHigh) / 2);
             userResponse = await ask(`Is your number ${guess}? Y or N \n`)
           }
-
   } // closes while
+    console.log(`I guessed your number, ${guess}!`);
 
-  console.log(`I guessed your number, ${guess}!`);
 
-} // closes else 
+  } // closes else for starting the game 
+
   process.exit();
 } // closes async start function 
